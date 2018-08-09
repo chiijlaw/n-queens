43bb4a771415d10af732711c54a832c26873d699
chiijlaw <chiithird@gmail.com>
2018-08-08 19:18:26 -0700

    Impl findNRooksSolution func body

--- src/solvers.js
+++ src/solvers.js
@@ -16,18 +16,23 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
+  var solution = new Board({n:n});
+  var rowIndex = 0;
+  var columnIndex = 0;
+  while (rowIndex < n && columnIndex < n) {
+    solution.togglePiece(rowIndex, columnIndex);
+    columnIndex += 1;
+    rowIndex += 1;
+  }
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var solutionCount = undefined; //fixme
-
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  return solutionCount; // should return a number
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
